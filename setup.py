98a6fae3b2183cb189a998fd847131ae0a6aea55
Daniel Palmer <daniel@0x0f.com>
2019-02-19 00:08:36 +0900

    Make module installable via pip. Fix a typo.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lx16a',
+      version='0.1',
+      author='Ethan Lipson',
+      author_email='ethan.lipson@gmail.com',
+      license='dunno',
+      packages=['lx16a'],
+      zip_safe=False)
